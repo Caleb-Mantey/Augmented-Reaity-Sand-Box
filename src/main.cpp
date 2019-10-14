2c03ff502912ef30e4d38d7712b808e61dc6756d
Caleb Mantey <manteycaleb@gmail.com>
2019-10-14 18:49:12 +0000

    Updated project for visual studio 2019

--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 #include "ofMain.h"
 #include "ofApp.h"
+#include "../../../../libs/glfw/include/GLFW/glfw3.h"
 
 const std::string MagicSandVersion = "1.5.4.2";
 
